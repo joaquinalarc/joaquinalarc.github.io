e79716501fb380c54044842b9a236057eb9583c7
Joaquin Alarcon <joaq.alrc@outlook.com>
2025-05-17 21:56:46 -0400

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function filterItems(category) {
     var items = document.getElementsByClassName('cert');
+    var buttons = document.querySelectorAll('.filter-buttons button');
+    
+    // Quitar la clase 'active' de todos los botones
+    buttons.forEach(button => button.classList.remove('active'));
+    
+    // Añadir la clase 'active' al botón seleccionado
+    document.getElementById(category).classList.add('active');
+
     for (var i = 0; i < items.length; i++) {
         if (category === 'all') {
             items[i].style.display = 'block';
